fbd6c380f627760a37a09da499a7101c070cc10c
unknown <jan@wendel.berlin>
2024-09-09 18:12:29 +0300

    new converse endpoint, scripts folder

--- vct-backend/app.py
+++ vct-backend/app.py
@@ -17,23 +17,9 @@ def converse():
     # We'll echo the json body back to the user in a 'user' key.
     return ml.converse(user_as_json['text'])
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/converse_new', methods=['POST'])
+def converse_new():
+    # This is the JSON body the user sent in their POST request.
+    user_as_json = app.current_request.json_body
+    # We'll echo the json body back to the user in a 'user' key.
+    return ml.converse_new(user_as_json)
--- vct-backend/ml.py
+++ vct-backend/ml.py
@@ -4,7 +4,11 @@ from botocore.exceptions import ClientError
 
 
 client = boto3.client("bedrock-runtime", region_name="eu-central-1")
-model_id = "amazon.titan-text-lite-v1"
+model_id = "anthropic.claude-v2"
+
+system_prompt = [{
+    "text": "You are Yapperino, a helpful assistant focues on anwsering questions about professional Valorant esports.",
+}]
 
 def converse(text):
     # Start a conversation with the user message.
@@ -15,14 +19,40 @@ def converse(text):
             "content": [{"text": user_message}],
         }
     ]
-
+    print(conversation)
     try:
         # Send the message to the model, using a basic inference configuration.
         response = client.converse(
-            modelId="amazon.titan-text-lite-v1",
+            modelId=model_id,
             messages=conversation,
             inferenceConfig={"maxTokens":1024,"stopSequences":["User:"],"temperature":0,"topP":1},
-            additionalModelRequestFields={}
+            additionalModelRequestFields={},
+            system=system_prompt,
+        )
+
+        # Extract and print the response text.
+        response_text = response["output"]["message"]["content"][0]["text"]
+        return response_text
+
+    except (ClientError, Exception) as e:
+        print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+        exit(1)
+
+
+def converse_new(conversation_log: list[(str, str)]):    
+    conversation = [{
+        "role": i[0],
+        "content": [{"text": i[1]}],
+    } for i in conversation_log]
+
+    print(conversation)
+    try:
+        response = client.converse(
+            modelId=model_id,
+            messages=conversation,
+            inferenceConfig={"maxTokens":1024,"stopSequences":["User:"],"temperature":0,"topP":1},
+            additionalModelRequestFields={},
+            system=system_prompt,
         )
 
         # Extract and print the response text.
