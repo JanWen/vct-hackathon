a9a7736c5d3544f7d501dff1eb2158c7bca4201d
unknown <jan@wendel.berlin>
2024-09-15 17:32:30 +0300

    converse_agent endpoint

--- vct-backend/app.py
+++ vct-backend/app.py
@@ -1,5 +1,5 @@
 from chalice import Chalice
-
+import uuid
 import ml
 
 app = Chalice(app_name='vct-backend')
@@ -9,6 +9,9 @@ app = Chalice(app_name='vct-backend')
 def index():
     return {'hello': 'world'}
 
+@app.route("/get_session_id", methods=['GET'])
+def get_session_id():
+    return str(uuid.uuid1())
 
 @app.route('/converse', methods=['POST'])
 def converse():
@@ -17,23 +20,13 @@ def converse():
     # We'll echo the json body back to the user in a 'user' key.
     return ml.converse(user_as_json['text'])
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/converse_agent', methods=['POST'])
+def converse():
+    # This is the JSON body the user sent in their POST request.
+    json_body = app.current_request.json_body
+    if "session_id" not in json_body:
+        return {"error": "session_id field is required"}
+    if "text" not in json_body:
+        return {"error": "text field is required"}
+    # We'll echo the json body back to the user in a 'user' key.
+    return ml.converse_agent(json_body['session_id'], json_body['text'])
